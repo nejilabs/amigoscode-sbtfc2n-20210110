639a9e2eae2630c124ff8bbd3d898670593abfe3
Nelson Alba Jr. 許鑫偉 (Nelly) <nelsonalbajr@yahoo.com>
2022-10-06 14:33:30 +0800

    @e 1806 - Student Class

--- amigoscodesbtfc2n20210110/src/main/java/com/amigoscode/amigoscodesbtfc2n20210110/Amigoscodesbtfc2n20210110Application.java
+++ amigoscodesbtfc2n20210110/src/main/java/com/amigoscode/amigoscodesbtfc2n20210110/Amigoscodesbtfc2n20210110Application.java
@@ -1,13 +1,28 @@
 package com.amigoscode.amigoscodesbtfc2n20210110;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Amigoscodesbtfc2n20210110Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Amigoscodesbtfc2n20210110Application.class, args);
+	}
+
+	// @GetMapping
+	// public String hello(){
+	// 	return "Hello World";
+	// }
+
+	@GetMapping
+	public List<String> hello(){
+		return List.of("Hello", "World");
 	}
 
 }
