8c01fa926c09ab4f238927ccb930ee74f0ac5010
Nelson Alba Jr. 許鑫偉 (Nelly) <nelsonalbajr@yahoo.com>
2022-10-06 15:58:14 +0800

    @e 2638 - Business Layer

--- amigoscodesbtfc2n20210110/src/main/java/com/amigoscode/amigoscodesbtfc2n20210110/Amigoscodesbtfc2n20210110Application.java
+++ amigoscodesbtfc2n20210110/src/main/java/com/amigoscode/amigoscodesbtfc2n20210110/Amigoscodesbtfc2n20210110Application.java
@@ -1,35 +1,12 @@
 package com.amigoscode.amigoscodesbtfc2n20210110;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.amigoscode.amigoscodesbtfc2n20210110.student.Student;
-
 @SpringBootApplication
-@RestController
 public class Amigoscodesbtfc2n20210110Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Amigoscodesbtfc2n20210110Application.class, args);
-	}
-
-	@GetMapping
-	public List<Student> hello(){
-		return List.of(
-			new Student(
-				1L,
-				"Mariam",
-				"mariam.jamal@gmail.com",
-				LocalDate.of(2000,Month.JANUARY,5),
-				21
-			)
-		);
 	}
 
 }
