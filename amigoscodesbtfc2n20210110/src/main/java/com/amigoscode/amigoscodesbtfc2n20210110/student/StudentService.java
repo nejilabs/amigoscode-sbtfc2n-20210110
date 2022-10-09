c3e1ff2efd5a106747ba140103fafe7eba1bcc85
Nelson Alba Jr. 許鑫偉 (Nelly) <nelsonalbajr@yahoo.com>
2022-10-09 15:44:55 +0800

    12133 - Exercise & Solution (Update Students Info) @e  12654 - Testing

--- amigoscodesbtfc2n20210110/src/main/java/com/amigoscode/amigoscodesbtfc2n20210110/student/StudentService.java
+++ amigoscodesbtfc2n20210110/src/main/java/com/amigoscode/amigoscodesbtfc2n20210110/student/StudentService.java
@@ -1,9 +1,10 @@
 package com.amigoscode.amigoscodesbtfc2n20210110.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -45,5 +46,30 @@ public class StudentService {
 		}
 
 		studentRepository.deleteById(studentId);
+	}
+
+	@Transactional
+	public void updateStudent(
+		Long studentId,
+		String name,
+		String email
+	){
+		Student student = studentRepository.findById(studentId)
+		.orElseThrow(() -> new IllegalStateException(
+			"student with id " + studentId + " does not exist"
+		));
+
+		if(name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+			student.setName(name);
+		}
+
+		if(email != null && email.length() > 0 && !Objects.equals(student.getEmail(),email)){
+			Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+			if(studentOptional.isPresent()){
+				throw new IllegalStateException("Email already taken");
+			}
+			student.setEmail(email);
+		}
 	}
 }
