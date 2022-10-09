3a0827f19ca6d08ab18f5ff4513ba6f4a9bb606d
Nelson Alba Jr. 許鑫偉 (Nelly) <nelsonalbajr@yahoo.com>
2022-10-09 11:53:55 +0800

    @e 10800 - Writing Business Logic

--- amigoscodesbtfc2n20210110/src/main/java/com/amigoscode/amigoscodesbtfc2n20210110/student/StudentService.java
+++ amigoscodesbtfc2n20210110/src/main/java/com/amigoscode/amigoscodesbtfc2n20210110/student/StudentService.java
@@ -20,16 +20,10 @@ public class StudentService {
 	}
 
     public List<Student> getStudents(){
-		// return List.of(
-		// 	new Student(
-		// 		1L,
-		// 		"Mariam",
-		// 		"mariam.jamal@gmail.com",
-		// 		LocalDate.of(2000,Month.JANUARY,5),
-		// 		21
-		// 	)
-		// );
-
 		return studentRepository.findAll();
+	}
+
+	public void addNewStudent(Student student){
+		System.out.println(student);
 	}
 }
