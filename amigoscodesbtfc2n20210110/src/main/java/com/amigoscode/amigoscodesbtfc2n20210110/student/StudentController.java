2bb97dc791734fd70d74c085939205e5ef236776
Nelson Alba Jr. 許鑫偉 (Nelly) <nelsonalbajr@yahoo.com>
2022-10-09 15:03:26 +0800

    @e 12133 - Exercise

--- amigoscodesbtfc2n20210110/src/main/java/com/amigoscode/amigoscodesbtfc2n20210110/student/StudentController.java
+++ amigoscodesbtfc2n20210110/src/main/java/com/amigoscode/amigoscodesbtfc2n20210110/student/StudentController.java
@@ -1,7 +1,9 @@
 package com.amigoscode.amigoscodesbtfc2n20210110.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +32,11 @@ public class StudentController {
 	@PostMapping
 	public void registerNewStudent(@RequestBody Student student){
 		studentService.addNewStudent(student);
+	}
+
+	@DeleteMapping(path = "{studentId}")
+	public void deleteStudent(@PathVariable("studentId") Long studentId){
+		studentService.deleteStudent(studentId);
 	}
 
 }
